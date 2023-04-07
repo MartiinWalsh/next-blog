287abec9cd300ba9fc9d76163126288f16234465
martinw-dev <martinwalsh.dev@gmail.com>
2023-04-07 20:39:30 +0100

    App layout and auth

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -17,10 +16,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const googleAuthProvider = new GoogleAuthProvider();
+const signInWithGoogleFirebase = () => signInWithPopup(auth, googleAuthProvider);
+
 const firestore = getFirestore(app);
 const storage = getStorage(app);
 
 // Export the initialized services
-export { analytics, auth, firestore, storage };
+export { auth, firestore, storage, googleAuthProvider, signInWithGoogleFirebase };
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,1 +1,30 @@
+import { collection, doc, onSnapshot } from "firebase/firestore";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from "react";
+import { auth, firestore } from "@/lib/firebase";
+
+export function useUserData() {
+  const [user] = useAuthState(auth);
+  const [username, setUsername] = useState("test");
+
+  useEffect(() => {
+    // turn off realtime subscription
+    let unsubscribe;
+    try {
+      if (user) {
+        const ref = doc(collection(firestore, "users"), user.uid);
+
+        unsubscribe = onSnapshot(ref, (doc) => {
+          //setUsername(doc.data()?.username);
+        });
+      } else {
+      }
+      return unsubscribe;
+    } catch (error) {
+      console.log(error);
+    }
+  }, [user]);
+
+  return { user, username };
+}
